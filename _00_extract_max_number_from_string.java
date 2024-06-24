5bf2baabc79b4e1e6efc06f0548d2395e5ec4131
Sameer Sawant <sawantsameer666@gmail.com>
2024-06-24 23:10:52 +0530

    max product of sub array

---
+++ _00_extract_max_number_from_string.java
@@ -0,0 +1,40 @@
+//Given a sentence containing several words and numbers. Find the largest number among them which does not contain 9. If no such number exists, return -1.
+//
+//Note: Numbers and words are separated by spaces only. It is guaranteed that there are no leading zeroes in the answer.
+//
+//Examples :
+//
+//Input: sentence="This is alpha 5057 and 97"
+//Output: 5057
+//Explanation: 5057 is the only number that does not have a 9.
+//Input: sentence="Another input 9007"
+//Output: -1
+//Explanation: Since there is no number that does not contain a 9,output is -1.
+
+public class _00_extract_max_number_from_string {
+    public long ExtractNumber(String sentence) {
+        // code here
+        long max = -1;
+        int i = 0;
+        int n = sentence.length();
+        while(i<n){
+            if(sentence.charAt(i) >= '0' && sentence.charAt(i) <= '9'){
+                long number = 0;
+                boolean notNine = true;
+                while(i<n && sentence.charAt(i) >= '0' && sentence.charAt(i) <= '9'){
+                    if(sentence.charAt(i)=='9'){
+                        notNine=false;
+                    }
+                    number = number * 10 + (sentence.charAt(i) - '0');
+                    i++;
+                }
+                if(notNine && number>max){
+                    max = number;
+                }
+            }else{
+                i++;
+            }
+        }
+        return max;
+    }
+}
