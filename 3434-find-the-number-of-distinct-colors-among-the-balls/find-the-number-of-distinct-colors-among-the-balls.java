8b56b137ab8852a355ca8ed76ba8073c6807e86a
Sameer Sawant <69522317+Sameersawant77@users.noreply.github.com>
2025-02-07 17:19:28 +0530

    Time: 51 ms (13.54%) | Memory: 82.1 MB (81.66%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
         Map<Integer,Integer> balls = new HashMap<>(), colors = new HashMap<>();
-        int n = queries.length, distinct = 0;
+        int n = queries.length;
         int[] res = new int[n];
         for(int i=0;i<n;i++){
             int ball = queries[i][0], color = queries[i][1];
             if(balls.containsKey(ball)){
-                int c = balls.get(ball);
-                int cnt = colors.get(c)-1;
-                if(cnt==0){
-                    colors.remove(c);
-                    distinct--;
+                if(balls.get(ball)==color){
+                    res[i] = colors.size();
+                    continue;
                 } 
-                else colors.put(c,cnt);
+                else if(colors.get(balls.get(ball))<=1){
+                    colors.remove(balls.get(ball));
+                } 
+                else{
+                    colors.put(balls.get(ball),colors.get(balls.get(ball))-1);
+                }
+                balls.put(ball,color);
+                colors.put(color,colors.getOrDefault(color,0)+1);
             }
-            balls.put(ball,color);
-            int cnt = colors.getOrDefault(color,0)+1;
-            colors.put(color,cnt);
-            if(cnt==1) distinct++;
-            res[i] = distinct;
+            else{
+                balls.put(ball,color);
+                colors.put(color,colors.getOrDefault(color,0)+1);
+            }
+            res[i] = colors.size();
         }
         return res;
     }
