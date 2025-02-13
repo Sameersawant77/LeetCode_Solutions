d3bf71916ec1547cb5207898f98e29cbf8bcf4ef
Sameer Sawant <69522317+Sameersawant77@users.noreply.github.com>
2025-02-13 11:40:17 +0530

    Time: 148 ms (69.14%) | Memory: 70.5 MB (80.25%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -3,13 +3,12 @@ class Solution {
         int count = 0;
         PriorityQueue<Long> pq = new PriorityQueue<>();
         for(int i=0;i<nums.length;i++){
-            pq.offer((long)nums[i]);
+            pq.add((long)nums[i]);
         }
         while(pq.size()>=2 && pq.peek()<k){
-            long num1 = pq.poll();
-            long num2 = pq.poll();
-            long op = (num1 * 2) + num2;
-            pq.offer(op);
+            long num1 = pq.remove();
+            long num2 = pq.remove();
+            pq.offer(num1 * 2 + num2);
             count++;
         }
         return pq.peek() >= k ? count : -1;
