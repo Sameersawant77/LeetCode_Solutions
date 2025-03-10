e4ee025263b434394a5977d44553e50518fda924
Sameer Sawant <69522317+Sameersawant77@users.noreply.github.com>
2025-03-10 13:27:03 +0530

    Time: 53 ms (100.00%) | Memory: 45.9 MB (90.60%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -3,45 +3,36 @@ class Solution {
         int[][] hash = new int[2][26];
         int currentK = 0, vowels = 0, extraLeft = 0;
         long res = 0;
-
-        // Mark vowels in hash[0]
-        hash[0]['a' - 'a'] = 1;
-        hash[0]['e' - 'a'] = 1;
-        hash[0]['i' - 'a'] = 1;
-        hash[0]['o' - 'a'] = 1;
-        hash[0]['u' - 'a'] = 1;
-
-        for (int right = 0, left = 0; right < word.length(); right++) {
+        hash[0]['a'-'a'] = 1;
+        hash[0]['e'-'a'] = 1;
+        hash[0]['i'-'a'] = 1;
+        hash[0]['o'-'a'] = 1;
+        hash[0]['u'-'a'] = 1;
+        for(int right=0,left=0; right<word.length();right++){
             char rightChar = word.charAt(right);
-
-            // Check if the character is a vowel or a consonant
-            if (hash[0][rightChar - 'a'] == 1) {
-                if (++hash[1][rightChar - 'a'] == 1) vowels++;
-            } else {
-                currentK++;
+            if(hash[0][rightChar-'a']==1){
+                if(++hash[1][rightChar-'a']==1) vowels++;
             }
+            else currentK++;
 
-            // Shrink the window if consonants exceed k
-            while (currentK > k) {
+            while(currentK>k){
                 char leftChar = word.charAt(left);
-                if (hash[0][leftChar - 'a'] == 1) {
-                    if (--hash[1][leftChar - 'a'] == 0) vowels--;
-                } else {
-                    currentK--;
+                if(hash[0][leftChar-'a']==1){
+                    if(--hash[1][leftChar-'a']==0) vowels--;
                 }
+                else currentK--;
                 left++;
-                extraLeft = 0; // Reset extraLeft when left pointer moves
+                extraLeft=0;
             }
 
-            // Recalculate extraLeft by moving left forward over duplicate vowels
             while (vowels == 5 && currentK == k && left < right && hash[0][word.charAt(left) - 'a'] == 1 && hash[1][word.charAt(left) - 'a'] > 1) {
-                extraLeft++;
-                hash[1][word.charAt(left) - 'a']--;
-                left++;
-            }
+    extraLeft++;
+    hash[1][word.charAt(left) - 'a']--;
+    left++;
+}
 
-            // If all vowels and k consonants exist, count substrings
-            if (vowels == 5 && currentK == k) {
+
+            if(vowels==5 && currentK==k){
                 res += (1 + extraLeft);
             }
         }
