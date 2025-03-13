1f0f5f35d6ddc3d92c93f57413c7c3d93a65669d
Sameer Sawant <69522317+Sameersawant77@users.noreply.github.com>
2025-03-13 13:16:20 +0530

    Time: 19 ms (55.24%) | Memory: 100.9 MB (91.78%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,21 +1,31 @@
 class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
-        int sum = 0, n = nums.length, k = 0;
-        int[] prefixSum = new int[n+1];
-        for(int i=0;i<nums.length;i++){
-            while(sum+prefixSum[i]<nums[i]){
-                if(k==queries.length) return -1;
-                int left = queries[k][0];
-                int right = queries[k][1];
-                int val = queries[k][2];
-                if(i<=right){
-                    prefixSum[Math.max(i,left)]+=val;
-                    prefixSum[right+1]-=val;
-                }
-                k++;
-            }
-            sum += prefixSum[i];
+        int n = nums.length;
+        
+        if (Arrays.stream(nums).allMatch(x -> x == 0)) return 0;
+        int left = 1, right = queries.length;
+        if (!canMakeZeroArray(right, nums, queries)) return -1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canMakeZeroArray(mid, nums, queries)) right = mid;
+            else left = mid + 1;
         }
-        return k;
+        return right;
+    }
+
+    private boolean canMakeZeroArray(int k, int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] diff = new int[n + 1];
+        for (int i = 0; i < k; i++) {
+            int left = queries[i][0], right = queries[i][1], val = queries[i][2];
+            diff[left] += val;
+            diff[right + 1] -= val;
+        }
+        int currVal = 0;
+        for (int i = 0; i < n; i++) {
+            currVal += diff[i];
+            if (currVal < nums[i]) return false;
+        }
+        return true;
     }
 }
