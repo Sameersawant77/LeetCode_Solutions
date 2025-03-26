a28dbb49276801672f79150cda8b42f4f1ab5276
Sameer Sawant <69522317+Sameersawant77@users.noreply.github.com>
2025-03-26 18:13:51 +0530

    Time: 5 ms (84.07%) | Memory: 44 MB (88.33%) - LeetSync

--- 1483-rank-teams-by-votes/rank-teams-by-votes.java
+++ 1483-rank-teams-by-votes/rank-teams-by-votes.java
@@ -1,37 +1,31 @@
 class Solution {
     public String rankTeams(String[] votes) {
         int n = votes[0].length();
-        Node[] rank = new Node[26];
-        for(int i=0;i<26;i++){
-            rank[i] = new Node((char) (i + 'A'));
-        }
+        int[][] rank = new int[26][n];
         for(String vote : votes){
             for(int i=0;i<n;i++){
-                rank[vote.charAt(i)-'A'].count[i]++;
+                rank[vote.charAt(i)-'A'][i]++;
             }
         }
-
-        Arrays.sort(rank,(a,b)->{
-            for(int i=0;i<26;i++){
-                if(a.count[i]!=b.count[i]){
-                    return b.count[i] - a.count[i];
+        Character[] teams = new Character[n];
+        for(int i=0;i<n;i++){
+            teams[i] = votes[0].charAt(i);
+        }
+        Arrays.sort(teams,(a,b)->{
+            int indexA = a - 'A';
+            int indexB = b - 'A';
+            for(int i=0;i<n;i++){
+                if(rank[indexA][i]!=rank[indexB][i]){
+                    return rank[indexB][i] - rank[indexA][i];
                 }
             }
-            return a.ch - b.ch;
+            return indexA - indexB;
         });
 
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<n;i++){
-            sb.append(rank[i].ch);
+            sb.append(teams[i]);
         }
         return sb.toString();
-    }
-}
-
-class Node{
-    char ch;
-    int[] count = new int[26];
-    Node(char ch){
-        this.ch = ch;
     }
 }
