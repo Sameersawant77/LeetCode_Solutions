47088db3e20b2935774ab26c62b51a4aa6d2925b
Sameer Sawant <69522317+Sameersawant77@users.noreply.github.com>
2025-03-27 12:11:24 +0530

    Time: 11 ms (95.51%) | Memory: 63.3 MB (14.74%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,26 +1,24 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        Map<Integer,Integer> freq = new HashMap<>();
-        int dominant = 0, maxCount = 0, n = nums.size();
+        int dominant = 0, count = 0, n = nums.size();
+        for(int num:nums){
+            if(count==0) dominant = num;
+            if(num==dominant) count++;
+            else count--;
+        }
+        count = 0;
+        for(int num:nums){
+            if(num==dominant) count++;
+        }
+        if(count<=(n>>1)) return -1;
+        int currentCount = 0;
         for(int i=0;i<n;i++){
             int num = nums.get(i);
-            freq.put(num,freq.getOrDefault(num,0)+1);
-            if(freq.get(num)>maxCount){
-                maxCount = freq.get(num);
-                dominant = num;
-            }
-        }
-        if(maxCount<n/2) return -1;
-        int currentCount = 0;
-        for(int i=0;i<n-1;i++){
-            int num = nums.get(i);
-            if(num==dominant){
+            if(num==dominant) {
                 currentCount++;
-                maxCount--;
-            } 
-            if (currentCount > (i + 1) / 2 && maxCount > (n - i - 1) / 2) {
-                return i;
+                count--;
             }
+            if(currentCount>((i+1)>>1) && count>((n-i-1)>>1)) return i;
         }
         return -1;
     }
