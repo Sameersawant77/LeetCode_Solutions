27a0eaa954b770086a7ceb556c3ec6df40f02ab8
Sameer Sawant <69522317+Sameersawant77@users.noreply.github.com>
2025-05-20 13:19:18 +0530

    Time: 2 ms (100.00%) | Memory: 95.8 MB (37.30%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
-        int[] freq = new int[n];
+        int[] lineBreak = new int[n+1];
         for(int[] query : queries){
-            freq[query[0]]++;
-            if(query[1]+1<n) freq[query[1]+1]--;
+            lineBreak[query[0]]++;
+            lineBreak[query[1]+1]--;
         }
-        int curFreq = 0;
+        int count = 0;
         for(int i=0;i<n;i++){
-            curFreq += freq[i];
-            if(curFreq<nums[i]) return false;
+            count += lineBreak[i];
+            if(nums[i]>count) return false;
         }
         return true;
     }
